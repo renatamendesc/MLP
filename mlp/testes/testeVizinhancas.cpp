4b6a962ecc3bb8fe4dfbdc708430a92921fc4267
Renata Mendes <renatampc030802@gmail.com>
2021-04-14 19:18:05 -0300

    Revisando

--- mlp/testes/testeVizinhancas.cpp
+++ mlp/testes/testeVizinhancas.cpp
@@ -20,6 +20,35 @@ int main(){
   vector <int > solucao = solucaoInicial;
   vector <int > vizinho = solucaoInicial;
 
+  for(int i = 1; i < tam-2; i++){
+    for(int j = 1; j <= tam-3; j++){
+      if(i != j){
+        if(i < j){
+          solucao.insert(solucao.begin() + j + 2, solucao[i]); 
+          solucao.insert(solucao.begin() + j + 3, solucao[i+1]); 
+          solucao.erase(solucao.begin() + i);
+          solucao.erase(solucao.begin() + i);
+        } else {
+          solucao.insert(solucao.begin() + j, solucao[i]); 
+          solucao.insert(solucao.begin() + j + 1, solucao[i + 2]); 
+          solucao.erase(solucao.begin() + i + 2);
+          solucao.erase(solucao.begin() + i + 2);
+        }
+
+        printf("Vizinho[%d][%d]: ", i, j);
+        for(int l = 0; l < tam; l++){
+          printf("%d ", solucao[l]);
+        }
+
+        cout << endl;
+
+        solucao = solucaoInicial;
+        vizinho = solucaoInicial;
+      }
+    }
+  } 
+
+  /*
   // Reinsertion:
   for(int i = 1; i < tam-2; i++){
     for(int j = i + 1; j < tam-1; j++){
@@ -56,7 +85,7 @@ int main(){
       vizinho = solucaoInicial;
     }
   }
-  
+  */
 
   /*
   // Or-opt-3:
