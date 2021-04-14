1a6517a98fb1a2da8bf3b0e83390856702313709
Renata Mendes <renatampc030802@gmail.com>
2021-04-14 17:46:30 -0300

    Revisando or-opt-2

--- mlp/testes/testeVizinhancas.cpp
+++ mlp/testes/testeVizinhancas.cpp
@@ -12,7 +12,7 @@ struct subseqInfo{
 };
 
 int main(){
-  //vector <vector <subseqInfo>> matrizSubseq(dimension+1, vector <subseqInfo> (dimension+1));
+
   vector <int> solucaoInicial = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1};
 
   double custoParcial, custo = 0, tempoParcial, tempo = 0;
@@ -20,6 +20,45 @@ int main(){
   vector <int > solucao = solucaoInicial;
   vector <int > vizinho = solucaoInicial;
 
+  // Reinsertion:
+  for(int i = 1; i < tam-2; i++){
+    for(int j = i + 1; j < tam-1; j++){
+      solucao.erase(solucao.begin()+i);
+      solucao.insert(solucao.begin()+j, solucaoInicial[i]);
+
+      printf("Vizinho[%d][%d]: ", i, j);
+      for(int l = 0; l < tam; l++){
+        printf("%d ", solucao[l]);
+      }
+
+      cout << endl;
+
+      solucao = solucaoInicial;
+      vizinho = solucaoInicial;
+    }
+  }
+
+  cout << endl;
+
+  for(int j = 1; j < tam-2; j++){
+    for(int i = j + 1; i < tam-1; i++){
+      solucao.erase(solucao.begin()+i);
+      solucao.insert(solucao.begin()+j, solucaoInicial[i]);
+
+      printf("Vizinho[%d][%d]: ", i, j);
+      for(int l = 0; l < tam; l++){
+        printf("%d ", solucao[l]);
+      }
+
+      cout << endl;
+
+      solucao = solucaoInicial;
+      vizinho = solucaoInicial;
+    }
+  }
+  
+
+  /*
   // Or-opt-3:
   for(int i = 1; i < tam-3; i++){
     for(int j = 1; j <= tam-4; j++){
@@ -40,7 +79,7 @@ int main(){
           solucao.erase(solucao.begin() + i + 3);
         }
       }
-
+    
       printf("Vizinho[%d][%d]: ", i, j);
       for(int l = 0; l < tam; l++){
         printf("%d ", solucao[l]);
@@ -52,4 +91,7 @@ int main(){
       vizinho = solucaoInicial;
     }
   }
+  */
+
+  return 0;
 }
